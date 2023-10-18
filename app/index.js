b644dbf25dcdfb53f1c97730df3d789ac5e83e97
basseyna <112563812+basseyna@users.noreply.github.com>
2023-10-18 08:53:37 +0100

    init api

---
+++ app/index.js
@@ -0,0 +1,32 @@
+const express  = require('express');
+const bodyParser = require('body-parser');
+const Queue = require('bull');
+const onboardingRoutes = require('./routes/onboardingRoutes.js')
+const attendanceRoutes = require('./routes/AttendanceRoutes.js');
+require('dotenv').config()
+
+const EmailQueue = new Queue('email-queue', process.env.REDIS_URL);
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/api/health-check', (req, res) => res.status(200).send({ message: "health check successful"}))
+app.use('/api/onboarding', onboardingRoutes);
+
+app.use('/api/attendance', attendanceRoutes);
+
+
+EmailQueue.process(async job  => {
+    console.log(`Processing a new job with id ${job.id}`, job.data);
+
+    return Promise.resolve();
+})
+
+
+
+module.exports = {
+    app, 
+    EmailQueue
+}
